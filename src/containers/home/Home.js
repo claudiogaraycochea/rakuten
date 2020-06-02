65f5f5cd7ac72d07ca52153300735dfc6addd5fb
Claudio Garaycochea <computadoraweb@gmail.com>
2020-06-02 03:30:40 -0300

    Router - subfolder fixed

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1> Hola Mundo! </h1>
+      <h1>HOME</h1>
     </div>
   );
 };
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,32 @@
-import About from '../components/About';
+/* Containers */
+import Home from '../containers/home/Home';
+import Detail from '../containers/movie/detail/Detail';
+
+/* Layouts */
 import PublicLayout from '../layouts/publicLayout/PublicLayout';
 
 export const routes = [
   {
+    id: 3,
+    exact: true,
+    path: '/detail',
+    Component: Detail,
+    Layout: PublicLayout,
+    Access: 'Public',
+  },
+  {
     id: 1,
     exact: true,
     path: '/',
-    Component: About,
+    Component: Home,
+    Layout: PublicLayout,
+    Access: 'Public',
+  },
+  {
+    id: 0,
+    exact: true,
+    path: '*',
+    Component: Detail,
     Layout: PublicLayout,
     Access: 'Public',
   },
