1c9b1f0abcbd55ea136ad50188e54bf8f36b2c69
Claudio Garaycochea <computadoraweb@gmail.com>
2020-06-12 17:06:31 -0300

    General fixes at Prop-types

--- src/containers/home/HomeContainer.js
+++ src/containers/home/HomeContainer.js
@@ -1,21 +1,15 @@
 import { connect } from 'react-redux';
-
 import { getMovies } from '../../store/movie/MovieActions';
-
 import Home from './Home';
 
 const mapStateToProps = (state) => {
 	return {
-		// device: state.default.system.device,
 		fullData: state.default.movie.fullData,
 	};
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		setHeaderTitle: (headerTitle) => {
-			dispatch(setHeaderTitle(headerTitle));
-		},
 		getMovies: () => {
 			dispatch(getMovies());
 		},
